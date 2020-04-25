89567f0e3024ae2da1344b34674b827056e6ea37
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2020-04-25 21:05:13 +0200

    Changed from html to have better command line tool viewing

--- index.php
+++ index.php
@@ -1,42 +1,16 @@
 <?php
-  header('Content-Type: text/html; charset=utf-8');
+  header('Content-Type: text/plain; charset=utf-8');
+  $request = $_SERVER['REQUEST_METHOD'] . ' ' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . ' ' . $_SERVER['SERVER_PROTOCOL'];
+  $query = $_SERVER['QUERY_STRING'];
+  echo "$request\n\n";
+  foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+  }
+  echo "\n\n";
+  $body = file_get_contents('php://input');
+  if (preg_match('/[[:^print:]]/', $body)) {
+    echo "<<binary>>";
+  } else {
+    echo "$body";
+   }
 ?>
-<html>
-  <head>
-  <title>Request</title>
-  <style>
-    html {
-      font-family: monospace;
-    }
-    th {
-      text-align: left;
-    }
-  </style>
-  </head>
-  <body>
-    <p>
-    <table>
-      <?php
-        $request = $_SERVER['REQUEST_METHOD'] . ' ' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . ' ' . $_SERVER['SERVER_PROTOCOL'];
-        $query = $_SERVER['QUERY_STRING'];
-        echo "<tr><th>Request</th><th>$request</th></tr>\n";
-        foreach (getallheaders() as $name => $value) {
-          echo "<tr>";
-          echo "<td>$name</td><td>$value</td>";
-          echo "</tr>\n";
-        }
-      ?>
-    </table>
-    </p>
-    <p>
-      <?php
-          $body = file_get_contents('php://input');
-          if (preg_match('/[[:^print:]]/', $body)) {
-            echo "&lt;&lt;binary&gt;&gt;\n";
-          } else {
-            echo "$body\n";
-          }
-      ?>
-    </p>
-  </body>
-</html>
