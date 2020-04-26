19c20912aadc17e4fe7934afc7102139fb2b2e02
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2020-04-26 16:04:25 +0200

    Added permissive CORS headers so that this endpint can be useful for debugging CSRF tests.
    Added random cookie

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
   header('Content-Type: text/plain; charset=utf-8');
+  $cookieName = "RandomTestCookie";
+  if(!isset($_COOKIE[$cookieName])) {
+    setcookie($cookieName, md5(rand()), time()+3600, "", "request.0x7d7b.net", false, false);
+  }
   $request = $_SERVER['REQUEST_METHOD'] . ' ' . (isset($_SERVER['HTTPS']) ? 'https' : 'http') .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . ' ' . $_SERVER['SERVER_PROTOCOL'];
   $query = $_SERVER['QUERY_STRING'];
   echo "$request\n\n";
